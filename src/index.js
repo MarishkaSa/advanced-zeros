092daae47a0083f2251c43468281a9402dc26c52
MarishkaSa <borninsea@gmail.com>
2019-03-19 20:08:24 +0300

    feat: change variable

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function getZerosCount(number, base) {
-  zeroCount = Number.MAX_VALUE; 
-  for (var s = 2; s <= base; s++) {
+  let zeroCount = Number.MAX_VALUE; 
+  for (let s = 2; s <= base; s++) {
     if (base % s === 0) {
-      var count = 0;
+      let count = 0;
       while (base % s === 0) {
          base = base / s;     
          count++;
       }
-      var divd = Math.floor(number / s);
-      var val = 0;
+      let divd = Math.floor(number / s);
+      let val = 0;
       while (divd > 0) {
         val += divd;
         divd = Math.floor(divd / s);
