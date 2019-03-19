3285e8fcf198249c1a85038d806c8df257f103ec
MarishkaSa <borninsea@gmail.com>
2019-03-19 20:01:17 +0300

    feat: answer zero count

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  zeroCount = Number.MAX_VALUE; 
+  for (var s = 2; s <= base; s++) {
+    if (base % s === 0) {
+      var count = 0;
+      while (base % s === 0) {
+         base = base / s;     
+         count++;
+      }
+      var divd = Math.floor(number / s);
+      var val = 0;
+      while (divd > 0) {
+        val += divd;
+        divd = Math.floor(divd / s);
+      }      
+      zeroCount = Math.min(zeroCount, Math.floor(val / count));
+    }
+  }
+  return zeroCount;
 }
